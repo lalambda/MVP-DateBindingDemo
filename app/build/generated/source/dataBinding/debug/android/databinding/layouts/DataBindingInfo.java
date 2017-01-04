a94b0515a5ae21596aa1b22d36b6921b4f41ee73
shootkill <yaoyafeng@AdministratordeMacBook-Pro.local>
2017-01-04 10:55:00 +0800

    lambda
    create code

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="ba719c4c-9450-45d7-8ad8-58891c7bdf87", modulePackage="com.lambda.demo", sdkRoot="/Users/admin/Library/Android/sdk",layoutInfoDir="/Users/admin/Documents/workspace/MVP+DateBindingDemo/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
